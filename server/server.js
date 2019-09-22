e8da371814a9b081a9e86d150d95f4fe66f7f96a
fernando <ferbarrera_fraga@outlook.es>
2019-09-22 16:41:21 -0500

    Mongo variable heroku

--- server/server.js
+++ server/server.js
@@ -1,29 +1,35 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-// parse application/x-www-form-urlencoded
-// Using Node.js `require()`
 const mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
-app.use(require('./routes'));
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+
+// Configuración global de rutas
+app.use(require('./routes/index'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+  if (err) throw err;
+
+  console.log('Base de datos ONLINE');
+
 });
 
-mongoose.connect(process.env.URLDB,
-  { useNewUrlParser: true, useCreateIndex: true },
-  (err, res) => {
-    if (err) throw err;
-    console.log('BD ONLINE');
-});
+
 
 app.listen(process.env.PORT, () => {
-  console.log('ESCUCHANDO PUERTO ', process.env.PORT)
+  console.log('Escuchando puerto: ', process.env.PORT);
 });
