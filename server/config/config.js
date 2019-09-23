93616a27477f755c42fc76ba662992d97c3a7111
fernando <ferbarrera_fraga@outlook.es>
2019-09-22 23:37:18 -0500

    Google Auth

--- server/config/config.js
+++ server/config/config.js
@@ -32,6 +32,14 @@ if(process.env.NODE_ENV === 'dev'){
     urlDB = process.env.MONGO_URL;
 }
 
+const GOOGLE_KEY = "AIzaSyAy_ngw6KH0BQG_Lr8YLeYd7H94d3fjbkg";
+
 //para codificar el url del servidor remoto, se debe de hacer una variable de entorno 
 // segun a donde se vaya a subir ( hay distintas maneras dependiendo del sitio ya sea heroku o DigitalOcean)
 process.env.URLDB = urlDB;
+
+
+// ======================================
+// GOOGLE CLIENT ID
+// ======================================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '774411371229-96kppvt4tb2jklnn63bartl2jtc3sflc.apps.googleusercontent.com';
